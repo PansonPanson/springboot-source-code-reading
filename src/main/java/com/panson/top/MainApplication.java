39f1ec5ee2f71911a2a82377edbec8d0fc2f565a
panxing <panxing@deepwise.com>
2020-07-11 22:11:22 +0800

    init

---
+++ src/main/java/com/panson/top/MainApplication.java
@@ -0,0 +1,16 @@
+package com.panson.top;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author panson
+ */
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
